90fe3d374c8bcf2c27f15295ad1ec48df9637099
sdshuvo98 <hellosdshuvo@gmail.com>
2023-06-06 14:49:05 +0100

    day three

---
+++ week-one/day-three/tempCodeRunnerFile.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int ans = -1;
+    for (int i = a; i <= b; i++)
+    {
+        if (i % c == 0)
+        {
+            ans = i;
+            break;
+        }
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
